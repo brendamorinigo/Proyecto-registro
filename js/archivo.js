1abff9704e70e23e05fb4c4645c32bfe78f6cc09
brenda <brendaden767@gmail.com>
2022-01-08 14:41:54 -0300

    ciclos e iteraciones

--- js/archivo.js
+++ js/archivo.js
@@ -1,23 +1,32 @@
-let incognita=27;
-do {
-let pregunta= prompt("Te animas a adivinar que numero estoy pensando?").toUpperCase()
-if(pregunta=="NO"){
-    alert("Dale, no seas anti.")
-    incognita===27
-}
-if(pregunta=="SI"){
-    let numeroIngresado= Number(prompt("El numero que estoy pensando esta entre 0 y 50, cual crees que es?"))
-if(numeroIngresado<10){
-alert("Segui subiendo estas lejos.")
-}else if(numeroIngresado<=20){
-    alert("Segui subiendo te vas acercando.")
-}else if(numeroIngresado <27){
-    alert("CERCA CERCA.")
-}else if(numeroIngresado===27)
-alert("Excelente, lo adivinaste")
-else if(numeroIngresado>=28){
-    alert("Te pasaste.")
-}else{
-    alert("Ingresa el dato solicitado.")
-}}
-} while (incognita);
+/* let numero= Number(prompt("Ingrese el numero que desea multiplicar."))
+let contador= 1;
+
+do{
+let resultado= contador * numero;
+console.log( `Los resultados son:${numero}*${contador}=${resultado}`)
+contador++;
+} while(contador <= 10);
+
+
+ */
+
+let pregunta = prompt("Adivina el numero que estoy pensando, esta entre 1 y 10");
+/* while(pregunta!=7) {
+alert(`${pregunta} no es el numero correcto, sigue intentando.`)
+pregunta = prompt("Vuelve a intentarlo");
+} */
+
+do{
+    if(pregunta <6){
+        alert('Estas muy cerca')
+    }
+    else if (pregunta>7){
+    alert ('Casi, te pasaste')
+    }
+    else if(pregunta===7) {
+        alert("Lo adivinaste")
+    }
+    else{
+        alert( 'No a ingresado un dato')
+    } 
+} while(pregunta)
---
+++ js/borrador.js
@@ -0,0 +1,5 @@
+let pregunta = prompt("Adivina el numero que estoy pensando, esta entre 1 y 10");
+while(pregunta!=7) {
+alert(`${pregunta} no es el numero correcto, sigue intentando.`)
+pregunta = prompt("Vuelve a intentarlo");
+} 
