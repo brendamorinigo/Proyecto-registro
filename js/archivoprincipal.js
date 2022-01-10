3ec6afae0b3747991ca8ab0e26fe4d73643d2064
brenda <brendaden767@gmail.com>
2022-01-10 16:47:26 -0300

    Ciclos e iteraciones

--- js/archivoprincipal.js
+++ js/archivoprincipal.js
@@ -1,32 +1,24 @@
-/* let numero= Number(prompt("Ingrese el numero que desea multiplicar."))
-let contador= 1;
+let continuarViaje= true;
+let nafta=10;
 
-do{
-let resultado= contador * numero;
-console.log( `Los resultados son:${numero}*${contador}=${resultado}`)
-contador++;
-} while(contador <= 10);
-
-
- */
-
-let pregunta = prompt("Adivina el numero que estoy pensando, esta entre 1 y 10");
-/* while(pregunta!=7) {
-alert(`${pregunta} no es el numero correcto, sigue intentando.`)
-pregunta = prompt("Vuelve a intentarlo");
-} */
-
-do{
-    if(pregunta <6){
-        alert('Estas muy cerca')
+  do {
+    let preguntarSeguir= prompt(`Tienes ${nafta}ltrs de nafta, quieres seguir el viaje?`).toLocaleUpperCase();
+    if(preguntarSeguir=='NO'){
+        if(nafta > 9){
+            console.log("Te queda bastante aun.")
+            continuarViaje=false
+        }
+        if(nafta < 9){
+            console.log("Te quedan menos de 9ltrs")
+            continuarViaje=false
+        }
+        if(nafta==0) {
+            console.log("Vas a tener que empujar")
+            continuarViaje=false
+        }
     }
-    else if (pregunta>7){
-    alert ('Casi, te pasaste')
+    else if(preguntarSeguir=="SI"){
+        nafta--;
+        console.log(`Te quedan ${nafta}ltrs`)
     }
-    else if(pregunta===7) {
-        alert("Lo adivinaste")
-    }
-    else{
-        alert( 'No a ingresado un dato')
-    } 
-} while(pregunta)
+     } while (continuarViaje);
--- js/borrador.js
+++ js/borrador.js
@@ -1,5 +1,27 @@
-let pregunta = prompt("Adivina el numero que estoy pensando, esta entre 1 y 10");
+/* let pregunta = prompt("Adivina el numero que estoy pensando, esta entre 1 y 10");
 while(pregunta!=7) {
 alert(`${pregunta} no es el numero correcto, sigue intentando.`)
 pregunta = prompt("Vuelve a intentarlo");
 } 
+ */
+
+do {
+    let preguntaBatir = prompt("sigo batiendo? Y / N").toUpperCase();
+    if (preguntaBatir == "N") {
+      if (batidas > 3) {
+        console.log("se te corto la mayonesa");
+        continuarBatiendo = false;
+      } else if (batidas == 3) {
+        console.log("quedo perfecta");
+        continuarBatiendo = false;
+      } else if (batidas < 3) {
+        console.log("falto batir");
+        continuarBatiendo = false;
+      }
+    } else if (preguntaBatir == "Y") {
+      batidas++;
+      console.log(`Van ${batidas} batidas`);
+    } else {
+      console.log("ponte a batir!");
+    }
+  } while (continuarBatiendo);
