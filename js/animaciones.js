e2f5140ebba027ecef8f4654fbf6a00feab99004
brenda <brendaden767@gmail.com>
2022-02-15 23:01:34 -0300

    jquery

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,1 +1,7 @@
-$("h1").fadeIn(4000)
+$("h1").fadeIn(4000);
+
+$(document).ready(function(){
+    $('#container-h3').on('mouseover', function (){
+        $('#container-h3').animate({height:'250px'}, 1000)
+    })
+})
