5cbd3183c4555fc743074f5d02c17f1d16782ad4
brenda <brendaden767@gmail.com>
2022-02-26 08:22:30 -0300

    3er entrega

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* DOM/ VARIABLES*/
-const bici1= new DatosBici("PHILCO", "AMARIILO", 29, "ONCE")
+const bici1= new DatosBici("PHILCO", "AMARILLO", 29, "ONCE")
 const seccionResultado= document.querySelector("#resultadoBuscador");
 const selectMarca= document.querySelector("#selectMarca");
 const selectColor= document.querySelector("#selectColor");
 const selectRodado= document.querySelector("#selectRodado");
-const form= document.querySelector("#form");
 const mostrarTodo= document.querySelector("#todo");
 const btnAñadir= document.querySelector("#btn-cargar")
+
 
 /* eventos */
 btnAñadir.addEventListener('click', cargarDatos)
@@ -42,18 +42,29 @@ selectRodado.addEventListener('change', ()=>{
 const bicicletas= [];
 bicicletas.push(bici1);
 
+
+
 /* Funciones */
 function cargarDatos(e){
   e.preventDefault()
-  let marcaIngresada= document.querySelector("#marca-ingresada").value;
-  let colorIngresado= document.querySelector("#color-ingresado").value;
+  let marcaIngresada= document.querySelector("#marca-ingresada").value.toUpperCase();
+  let colorIngresado= document.querySelector("#color-ingresado").value.toUpperCase();
   let rodadoIngresado= document.querySelector("#rodado-ingresado").value;
-  let ubicacionIngresada= document.querySelector("#ubicacion-ingresada").value;
+  let ubicacionIngresada= document.querySelector("#ubicacion-ingresada").value.toUpperCase();
+  const form= document.querySelector("#formulario-agregar");
+  localStorage.setItem("Color", colorIngresado);
+  localStorage.setItem("marca", marcaIngresada);
+  localStorage.setItem("rodado", rodadoIngresado);
+  localStorage.setItem("ubicacion", ubicacionIngresada);
+
+
 
   const nuevoRegistro= new DatosBici(marcaIngresada,colorIngresado,rodadoIngresado,ubicacionIngresada);
   bicicletas.push(nuevoRegistro)
   imprimirProductos(bicicletas)
+  form.reset();
 }
+
 
 function imprimirProductos(array){
     seccionResultado.innerHTML=""
@@ -80,6 +91,25 @@ function imprimirProductos(array){
 imprimirProductos(bicicletas);
 
 
-
-
-
+function imprimirLocalStorage() { 
+  for (let i=0; i < localStorage.length; i++){
+    let clave= localStorage.key(i);
+    let div =document.createElement("div")
+        div.classList.add("containerRes")
+        div.innerHTML += 
+        `<div class="card" style="width: 18rem;">
+        <img src="./imagenes/3d39637d629db755b52cc80a4454.jpg" width="150px" height="200px" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">${localStorage.getItem("marca")}</li>
+          <li class="list-group-item">${localStorage.getItem("color")}</li>
+          <li class="list-group-item">${localStorage.getItem("rodado")}</li>
+        </ul>
+      </div>`
+      seccionResultado.appendChild(div)
+  }
+ }
+ imprimirLocalStorage();
